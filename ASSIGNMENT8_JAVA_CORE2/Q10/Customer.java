3ee906ec56d51d15ab65188bbb3eb4029b5f37d7
pooja-ttn <47346902+pooja-ttn@users.noreply.github.com>
2019-02-22 13:16:14 +0530

    ASSIGNMENT8_JAVA_CORE

---
+++ ASSIGNMENT8_JAVA_CORE2/Q10/Customer.java
@@ -0,0 +1,54 @@
+package Q10;
+
+
+public class Customer {
+    public String name;
+    public String pdt;
+    public int price;
+   private static int  token=190;
+/*
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }*/
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    Customer(String name) {
+        this.name=name;
+        pdt = "Latte";
+        price = 100;
+    }
+
+    public void order() {
+        System.out.println("\n"+name+"'s order");
+        System.out.println(pdt + " " + price+"/");
+    }
+
+    public int pay() {
+        System.out.println("\n"+name+" paid price "+price);
+        return price;
+    }
+
+
+
+
+    public void ready()
+    {
+                System.out.println("Order taken by Customer "+token);
+                     token++;
+            }
+
+
+
+
+}
