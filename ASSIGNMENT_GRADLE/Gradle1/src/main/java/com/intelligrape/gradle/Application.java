c50f5e42bd83f7b39418544ad7de35e4e4340894
pooja-ttn <pooja.joshi@tothenew.com>
2019-03-06 20:40:06 +0530

    Assignment_Gradle

---
+++ ASSIGNMENT_GRADLE/Gradle1/src/main/java/com/intelligrape/gradle/Application.java
@@ -0,0 +1,14 @@
+package com.intelligrape.gradle;
+
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Application {
+
+public static void main(String[] args) {
+        System.out.println("Hello World");
+        String str="Hello People";
+     System.out.println(StringUtils.reverse(str));
+    }
+
+}
